339d6583b84d68f22371e9c790eeaca06a34dacb
bencallis1 <bencallis1@gmail.com>
2016-03-29 13:45:12 -0600

    Changed the animating speed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 $('#add-todo').click(function() {
     TweenMax.to(".add-todo-button",.5, {x: -345, ease:Power1.easeInOut});
-    TweenMax.to(".complete-todo-button",.7, {opacity:1,scale:1,x: -345, ease:Power1.easeInOut});
+    TweenMax.to(".complete-todo-button",.45, {opacity:1,scale:1,x: -345, ease:Power1.easeInOut});
     TweenMax.to(".bottom-form-container",.5, {y: -345, ease:Power1.easeInOut});
 
 });
