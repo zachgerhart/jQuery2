ca1401260f944711f31995ed4904d3f9f9321e28
bencallis1 <bencallis1@gmail.com>
2016-03-30 14:53:07 -0600

    added the close modal functionality

--- js/animation.js
+++ js/animation.js
@@ -12,3 +12,10 @@ $('#saveNewItem').click(function() {
     TweenMax.to(".add-todo-button",.5, {x: 0, ease:Power1.easeInOut});
     TweenMax.to(".bottom-form-container",.5, {y: 345, ease:Power1.easeInOut});
 });
+
+
+
+$('#cancel').click(function() {
+    TweenMax.to(".add-todo-button",.5, {x: 0, ease:Power1.easeInOut});
+    TweenMax.to(".bottom-form-container",.5, {y: 345, ease:Power1.easeInOut});
+});
