9153d1a8de4a4727f04cfb85c59afef65dac9b22
bencallis1 <bencallis1@gmail.com>
2016-03-30 14:46:12 -0600

    added the javascript to make sure the todo worked

--- js/app.js
+++ js/app.js
@@ -1,13 +1,90 @@
+$(document).ready(function() {
+    //ALL CODE GOES IN HERE
+    var advanceTask = function(task) {
+        var modified = task.innerText.trim()
+        for (var i = 0; i < listo.length; i++) {
+            if (listo[i].task === modified) {
+                if (listo[i].id === 'new') {
+                    listo[i].id = 'inProgress';
+                } else if (listo[i].id === 'inProgress') {
+                    listo[i].id = 'archived';
+                } else {
+                    listo.splice(i, 1);
+                }
+                break;
+            }
+        }
+        task.remove();
+    };
+    $('#newTaskForm').hide();
 
-$('#add-todo').click(function() {
-    TweenMax.to(".add-todo-button",.5, {x: -345, ease:Power1.easeInOut});
-    TweenMax.to(".complete-todo-button",.45, {opacity:1,scale:1,x: -345, ease:Power1.easeInOut});
-    TweenMax.to(".bottom-form-container",.5, {y: -345, ease:Power1.easeInOut});
 
-});
+    var listo = [];
+    var Task = function(task) {
+        this.task = task;
+        this.id = "new";
+    }
+    var addTask = function(task) {
+        if(task) {
+            task = new Task(task);
+            listo.push(task);
 
-$('#complete-todo').click(function() {
-    TweenMax.to(".add-todo-button",.5, {x: 0, ease:Power1.easeInOut});
-    TweenMax.to(".complete-todo-button",.4, {opacity:1,scale:1,x: 0, ease:Power1.easeInOut});
-    TweenMax.to(".bottom-form-container",.5, {y: 345, ease:Power1.easeInOut});
+            $('#newItemInput').val('');
+
+            $('#newList').append('<a href="#finish" class="" id="item"><li class="list-group-item">' + task.task + '<span class="arrow pull-right"><i class="glyphicon glyphicon-arrow-right"></span></li></a>');
+
+        }
+        // $('#newTaskForm,  #newListItem').fadeToggle('slow', 'linear');
+
+    };
+    $('#saveNewItem').on('click', function (e) {
+        e.preventDefault();
+        var task = $('#newItemInput').val().trim();
+        addTask(task);
+    });
+//Opens form
+    $('#newListItem').on('click', function () {
+        $('#newTaskForm,  #newListItem').fadeToggle('slow', 'linear');
+    });
+    //closes form
+    $('#cancel').on('click', function (e) {
+        e.preventDefault();
+        $('#newTaskForm,  #newListItem').fadeToggle('slow', 'linear');
+    });
+    $(document).on('click', '#item', function(e) {
+        e.preventDefault();
+    });
+    $(document).on('click', '#item', function(e) {
+        e.preventDefault();
+        var task = this;
+        advanceTask(task);
+        this.id = 'inProgress';
+    });
+    $(document).on('click', '#item', function(e) {
+        e.preventDefault();
+        var task = this;
+        advanceTask(task);
+        this.id = 'inProgress';
+        $('#currentList').append(this.outerHTML);
+    });
+    $(document).on('click', '#inProgress', function (e) {
+        e.preventDefault();
+        var task = this;
+        task.id = "archived";
+        var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
+        advanceTask(task);
+        $('#archivedList').append(changeIcon);
+    });
+    $(document).on('click', '#archived', function (e) {
+        e.preventDefault();
+        var task = this;
+        advanceTask(task);
+    });
+// var listoSave = [ { id: 4, setting: 2 }, [ id:3, setting:1 ] };
+
+// $("#myitem").data("savelist", listoSave);
+
+
+
+
 });
